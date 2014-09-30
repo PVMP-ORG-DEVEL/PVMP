ef461dddbcf90d4a3279e4716d9e766ec3838cf7
Jonathan Rufino <jonathan_rufinotkd@hotmail.com>
2014-09-30 11:54:08 -0300

    Implemented user account edition

--- src/org/development/pvmp/EditSettingsFragment.java
+++ src/org/development/pvmp/EditSettingsFragment.java
@@ -2,10 +2,8 @@ package org.development.pvmp;
 
 import dao.UserDAO;
 import models.User;
-import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +17,9 @@ public class EditSettingsFragment extends Fragment{
 	private EditText editText_editName;
 	private EditText editText_editEmail;
 	private EditText editText_editAge;
-	private RadioGroup radioButton_editEducation;
+	private RadioGroup radioGroup_editEducation;
 	private EditText editText_oldPassword;
 	private EditText editText_newPassword;
-	private FragmentManager fragmentManager;
 	private Button button_save;
 	private MainActivity mainActivity;
 	private User loggedUser;
@@ -40,23 +37,68 @@ public class EditSettingsFragment extends Fragment{
 		editText_editName = (EditText) viewEditSettings.findViewById(R.id.editText_editName);
 		editText_editEmail = (EditText) viewEditSettings.findViewById(R.id.editText_editEmail);
 		editText_editAge = (EditText) viewEditSettings.findViewById(R.id.editText_editAge);
-		// radioButton_editEducation = (RadioGroup) viewEditSettings.findViewById(R.id.editText_editName);
+		radioGroup_editEducation = (RadioGroup) viewEditSettings.findViewById(R.id.radioGroup_editEducation);
 		editText_oldPassword = (EditText) viewEditSettings.findViewById(R.id.editText_oldPassword);
 		editText_newPassword = (EditText) viewEditSettings.findViewById(R.id.editText_newPassword);
 		
+		button_save = (Button) viewEditSettings.findViewById(R.id.button_save);
+		
 		clickSave();
+		setFieldData();
 		
 		return viewEditSettings;		
 	}
 	
 	public void clickSave () {
-		//UserDAO userDao = UserDAO.getInstance(mainActivity.getApplicationContext());
-		//userDao.edit(loggedUser);
+		
 		button_save.setOnClickListener(new OnClickListener () {
 			@Override
 			public void onClick(View v) {
+				
+				saveUserEdition();
+				UserDAO userDao = UserDAO.getInstance(mainActivity.getApplicationContext());
+				userDao.edit(loggedUser);
 				mainActivity.changeFragment(3);
 			}
 		});
-	}	
+	}
+	
+	public void setFieldData () {
+		editText_editName.setText(loggedUser.getName());
+		editText_editEmail.setText(loggedUser.getEmail());
+		editText_editAge.setText(Integer.toString((loggedUser.getAge())));
+		
+		if (loggedUser.getEducation() == "Fundamental")
+			radioGroup_editEducation.check(R.id.radio_editElementarySchool);
+		else if (loggedUser.getEducation() == "Medio")
+			radioGroup_editEducation.check(R.id.radio_editHighSchool);
+		else
+			radioGroup_editEducation.check(R.id.radio_editGraduated);
+	}
+	
+	public void saveUserEdition () {
+		loggedUser.setName(this.editText_editName.getText().toString());
+		loggedUser.setEmail(this.editText_editEmail.getText().toString());
+		loggedUser.setAge(Integer.parseInt(this.editText_editAge.getText().toString()));
+		
+		String education = null;
+		
+		switch (radioGroup_editEducation.getCheckedRadioButtonId()) {
+			case R.id.radio_editElementarySchool:
+				education = "Fundamental";
+				break;
+			case R.id.radio_editHighSchool:
+				education = "Medio";
+				break;
+			case R.id.radio_editGraduated:
+				education = "Superior";
+				break;	
+		}
+		
+		loggedUser.setEducation(education);
+		
+		if (!editText_oldPassword.getText().toString().equals("") &&
+				!editText_newPassword.getText().toString().equals(""))
+			loggedUser.setPassword(this.editText_newPassword.getText().toString());
+	}
 }
